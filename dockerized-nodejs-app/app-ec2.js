43999851bcc664c875ec1b3087a0266ae8541c06
mburakunuvar <mburakunuvar@gmail.com>
2021-06-22 11:54:59 +0200

    updated app code for new docker images

---
+++ dockerized-nodejs-app/app-ec2.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>Dockerized NodeJS Application running on Amazon ECS with EC2 Launch Type</h1>
+    <h2>This page is only for subscribed members of our webpage!</h2>
+    <ul>
+    <li> Resource utilization per user is known </li>
+    <li> Cluster Autoscaling and Idle EC2 Capacity is managed by us</li>
+    <li> We can SSH into host and install  proprietary company software</li>
+    </ul>
+    <img src="https://orchestrating-ecs-launch-types.s3.eu-west-1.amazonaws.com/ECS-html.png" alt="ECS with EC2 Launch Type" width="800" height="400">`
+  );
+});
+
+app.get("/ec2/*", (req, res) => {
+  res.send(
+    `<h2> running on Amazon ECS with EC2 Launch Type </h2>
+    <h3> http request to /ec2/* </h3>`
+  );
+});
+
+app.listen(80, () => {
+  console.log("Listening on port 80");
+});
---
+++ dockerized-nodejs-app/app-fargate.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>Dockerized NodeJS Application running on Amazon ECS with Fargate Launch Type</h1>
+    <h2>This is landing page of our web application, open to everyone!</h2>
+    <ul>
+    <li> We’re expecting a spike in traffic and unpredictable workload </li>
+    <li> Not enough data to predict resource utilization </li>
+    <li> Not enough time for manually maintaining and autoscaling of cluster. </li>
+    </ul>
+    <img src="https://orchestrating-ecs-launch-types.s3.eu-west-1.amazonaws.com/Fargate-html.png" alt="ECS with EC2 Launch Type" width="800" height="400">`
+  );
+});
+
+app.get("/ec2/*", (req, res) => {
+  res.send(
+    `<h2> running on Amazon ECS with EC2 Launch Type </h2>
+    <h3> http request to /ec2/* </h3>`
+  );
+});
+
+app.listen(80, () => {
+  console.log("Listening on port 80");
+});
