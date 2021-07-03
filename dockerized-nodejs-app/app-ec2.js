0f8d9d5e38f7b64affaa8b38390ceace950a26f1
Burak Unuvar <buraku@amazon.com>
2021-07-03 22:32:17 +0300

    updated path parameters and CFN outputs

--- dockerized-nodejs-app/app-ec2.js
+++ dockerized-nodejs-app/app-ec2.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send(
+    `<h1>Dockerized NodeJS Application running on Amazon ECS with EC2 Launch Type</h1>`
+  );
+});
+
+app.get("/ec2/*", (req, res) => {
+  res.send(
     `<h1>Dockerized NodeJS Application running on Amazon ECS with EC2 Launch Type</h1>
     <h2>This page is only for subscribed members of our webpage!</h2>
     <ul>
@@ -12,13 +18,6 @@ app.get("/", (req, res) => {
     <li> We can SSH into host and install  proprietary company software</li>
     </ul>
     <img src="https://orchestrating-ecs-launch-types.s3.eu-west-1.amazonaws.com/ECS-html.png" alt="ECS with EC2 Launch Type" width="800" height="400">`
-  );
-});
-
-app.get("/ec2/*", (req, res) => {
-  res.send(
-    `<h2> running on Amazon ECS with EC2 Launch Type </h2>
-    <h3> http request to /ec2/* </h3>`
   );
 });
 
--- dockerized-nodejs-app/app-fargate.js
+++ dockerized-nodejs-app/app-fargate.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send(
+    `<h1>Dockerized NodeJS Application running on Amazon ECS with Fargate Launch Type</h1>`
+  );
+});
+
+app.get("/fargate/*", (req, res) => {
+  res.send(
     `<h1>Dockerized NodeJS Application running on Amazon ECS with Fargate Launch Type</h1>
     <h2>This is landing page of our web application, open to everyone!</h2>
     <ul>
@@ -12,13 +18,6 @@ app.get("/", (req, res) => {
     <li> Not enough time for manually maintaining and autoscaling of cluster. </li>
     </ul>
     <img src="https://orchestrating-ecs-launch-types.s3.eu-west-1.amazonaws.com/Fargate-html.png" alt="ECS with EC2 Launch Type" width="800" height="400">`
-  );
-});
-
-app.get("/ec2/*", (req, res) => {
-  res.send(
-    `<h2> running on Amazon ECS with EC2 Launch Type </h2>
-    <h3> http request to /ec2/* </h3>`
   );
 });
 
